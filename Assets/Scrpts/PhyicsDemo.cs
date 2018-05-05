55dc114a1adc960f1b56c719857a97264802a3cb
berthmoon19930618 <berthmoon19930618@gmail.com>
2018-05-05 16:44:15 +0800

    20180505

--- Assets/Scrpts/PhyicsDemo.cs
+++ Assets/Scrpts/PhyicsDemo.cs
@@ -3,16 +3,33 @@ using System.Collections;
 
 public class PhyicsDemo : MonoBehaviour {
 
+    //Vector3 vect;//將xyz向量命名為vect
+    public GameObject crater;//彈痕
+
     //OnCollisionEnter 運動中物體開始碰撞時偵測一次
     //OnCollisionStay 運動中物體碰撞中每幀偵測一次
     //OnCollisionExit 運動中物體結束碰撞時偵測一次
     //只要滿足了碰撞條件都會執行以下方法：
+
     void OnCollisionExit(Collision collOther)
     {
         //collOther 通過參數可以獲取掛腳本其他方的物體
         //gameObject 遊戲物體
         //name 物體名稱
-        Debug.Log(collOther.gameObject.name);
-	}
+        // Debug.Log(collOther.gameObject.name);
 
+        //接獲第一個接觸點訊息
+        ContactPoint cp = collOther.contacts[0];
+       // vect = cp.normal;//法線
+
+        //1.要創建的物件 crater
+        //2.要創建在何處 cp.point
+        //3.要創建的角度 物體y軸正方向與法線方向一致
+        GameObject.Instantiate(crater, cp.point+cp.normal*0.03f, Quaternion.FromToRotation(Vector3.up,cp.normal));
+    }
+    //畫出法線
+    //void Update()
+    //{
+    //    Debug.DrawLine(Vector3.zero, vect);
+    //}
 }
